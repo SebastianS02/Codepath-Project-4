2dccf2978fe96aeefb05e18a2b021de45b5f87a7
Sebastian Stec <sebastianstec@sebastians-mbp.myfiosgateway.com>
2023-03-20 16:36:40 -0400

    API call displays three attributes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
 
-  const URL = `https://api.thedogapi.com/v1/images/search?api_key=live_dTInATzYCb1o75sVugm74LblSYxSjr0WaspyPa4lgX2zKma5C9MLev1pDxKSCYLs?has_breeds?include_breeds=true`;
+  const URL = `https://api.thedogapi.com/v1/images/search?api_key=live_dTInATzYCb1o75sVugm74LblSYxSjr0WaspyPa4lgX2zKma5C9MLev1pDxKSCYLs?has_breeds=true/include_breeds=true`;
 
   const [currentImage, setCurrentImage] = useState([]);
   const [currentWeight, setCurrentWeight] = useState([]);
@@ -14,18 +14,20 @@ function App() {
     fetch(URL).then((response) => response.json())
     .then((data) => {
       setCurrentImage(data[0]);
-      setCurrentWeight(data[0].breeds.weight);
-      setCurrentHeight(data[0].breeds.height);
-      setCurrentName(data[0].breeds.name);
-      console.log(currentImage);
+      setCurrentName(data[0].breeds[0].name);
+      setCurrentWeight(data[0].breeds[0].weight.imperial);
+      setCurrentHeight(data[0].breeds[0].height.imperial);
+      console.log(data[0]);
     })
   }
 
   return (
     <div className="whole-page">
       <h1>Veni Vici!</h1>
-      <h2>Weight: {currentWeight} Height: {currentHeight} Name: {currentName}</h2>
-        <img className="screenshot" src={currentImage ? currentImage.url : ''} alt="No current image!" />
+      <h2>Name: {currentName}</h2>
+      <h3>Height: {currentHeight}</h3>
+      <h3>Weight: {currentWeight}</h3>
+        <img className="screenshot" src={currentImage ? currentImage.url : 'No current image!'} />
         <br></br>
         <button onClick={apiCall}>🔄Find a new dog!🔄</button>
     </div>
